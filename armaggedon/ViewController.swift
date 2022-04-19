03fc06579107285ead56fd2a55c6b82930b8c72a
Kirill <kirilldrozd12@gmail.com>
2022-04-19 21:03:42 +0300

    Initial Commit

---
+++ armaggedon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  armaggedon
+//
+//  Created by Kirill Drozdov on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
